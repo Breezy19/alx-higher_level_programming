097c6d669ece034839346d75e0c7d2a6c9774ef1
Akram Freij <freij.akram@gmail.com>
2023-06-05 13:15:58 -0700

    0x00-python-hello_world

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+
+def print_art():
+    sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+
+if __name__ == "__main__":
+    print_art()
+    sys.exit(1)
