0e7a2afb8824617b4ab054a0bcf4d4fbdfd6c53c
Akram Freij <freij.akram@gmail.com>
2023-06-14 14:05:13 -0700

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,10 +1,41 @@
 #!/usr/bin/python3
-def roman_to_int(num):
-    listax = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    r = 0
-    for i,c in enumerate(num):
-        if (i+1) == len(num) or listax[c] >= listax[num[i+1]]:
-            r += listax[c]
-        else:
-            r -= listax[c]
-    return r
+def roman_to_int(roman_string):
+    if not roman_string:
+        return 0
+    if not isinstance(roman_string, str):
+        return 0
+    if not roman_string.isupper():
+        roman_string.upper()
+    r_dict = {
+        "I": 1,
+        "IV": 4,
+        "V": 5,
+        "IX": 9,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+
+    result = 0
+    temp = list(roman_string)
+    if len(temp) > 1:
+        idx = 0
+        for i in temp:
+            try:
+                if temp[idx] == 'I' and temp[idx + 1] == 'V':
+                    temp[idx:idx + 2] = [''.join(temp[idx:idx + 2])]
+            except IndexError:
+                pass
+            try:
+                if temp[idx] == 'I' and temp[idx + 1] == 'X':
+                    temp[idx:idx + 2] = [''.join(temp[idx:idx + 2])]
+            except IndexError:
+                pass
+            idx += 1
+    for k, v in r_dict.items():
+        for index in temp:
+            if index == k:
+                result += v
+    return result
